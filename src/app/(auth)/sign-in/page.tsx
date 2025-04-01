73ba8b7554b08d8a3418c9d256adfddf6c4c8e9e
Diego Coronado <dcoronado@able.co>
2025-04-01 17:40:53 +0900

    add auth page components

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,12 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <AuthForm type="sign-in" />
+    </div>
+  );
 };
 
 export default Page;
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import AuthForm from "@/components/AuthForm";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <AuthForm type="sign-up" />
+    </div>
+  );
+};
 
-export default Page
+export default Page;
